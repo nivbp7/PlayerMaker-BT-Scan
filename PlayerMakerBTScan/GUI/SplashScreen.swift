00bba1ba735baf7ae78a4d51013248e538b77735
Niv BP <nivbp7@gmail.com>
2025-08-26 11:06:18 +0300

    Add SplashScreen

---
+++ PlayerMakerBTScan/GUI/SplashScreen.swift
@@ -0,0 +1,24 @@
+//
+//  SplashScreen.swift
+//  PlayerMakerBTScan
+//
+//  Created by niv ben-porath on 26/08/2025.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground).ignoresSafeArea()
+            Text("Niv Ben-Porath")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.accent)
+        }
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
