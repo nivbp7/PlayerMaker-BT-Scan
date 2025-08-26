c4739666844030ae72486af3eba3ac4df3f29cc4
Niv BP <nivbp7@gmail.com>
2025-08-26 13:20:02 +0300

    Add demo devices and a method to get the name

--- PlayerMakerBTScan/LocalModels/Device.swift
+++ PlayerMakerBTScan/LocalModels/Device.swift
@@ -12,3 +12,15 @@ struct Device: Identifiable {
     let name: String
     var nickname: String?
 }
+
+extension Device {
+    static let demo = Device(id: "3cca58c0-0e93-4493-9e01-1278d67aef1", name: "Niv's iPhone 16")
+    static let demo2 = Device(id: "3cca58c0-0e93-4493-9e01-1278d67aef2", name: "Niv's iPh")
+    static let demoWithNickname = Device(id: "3cca58c0-0e93-4493-9e01-1278d67aef3", name: "Niv's iPhone 16", nickname: "My phone")
+}
+
+extension Device {
+    func shownName() -> String {
+        return nickname ?? name
+    }
+}
