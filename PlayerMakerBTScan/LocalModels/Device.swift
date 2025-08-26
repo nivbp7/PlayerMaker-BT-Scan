4387e4322b8fd8935191cc9c1a7800f673d4e9e3
Niv BP <nivbp7@gmail.com>
2025-08-26 10:58:48 +0300

    Add local Device model

---
+++ PlayerMakerBTScan/LocalModels/Device.swift
@@ -0,0 +1,14 @@
+//
+//  Device.swift
+//  PlayerMakerBTScan
+//
+//  Created by niv ben-porath on 26/08/2025.
+//
+
+import Foundation
+
+struct Device {
+    let id: String
+    let name: String
+    var nickname: String?
+}
