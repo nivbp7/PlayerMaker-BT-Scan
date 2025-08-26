70598dac0f810c04c5aa78a3ae5583945d7864cd
Niv BP <nivbp7@gmail.com>
2025-08-26 16:22:18 +0300

    Scan for BT devices

--- PlayerMakerBTScan/GUI/Views/ScanScreen/ScanView.swift
+++ PlayerMakerBTScan/GUI/Views/ScanScreen/ScanView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct ScanView: View {
+    
+    let bm = BluetoothManager()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("BT")
+        }
+        .onAppear(perform: {
+            bm.startScan()
+        })
     }
 }
 
--- PlayerMakerBTScan/Service/Bluetooth/BluetoothManager.swift
+++ PlayerMakerBTScan/Service/Bluetooth/BluetoothManager.swift
@@ -6,7 +6,52 @@
 //
 
 import Foundation
+import CoreBluetooth
 
-final class BluetoothManager {
+final class BluetoothManager: NSObject {
+    private var central: CBCentralManager!
+    private var authorization: AuthorizationStatus = .unknown
+    private var isScanning = false
 
+    enum AuthorizationStatus {
+        case allowed
+        case denied
+        case poweredOff
+        case unknown
+    }
+    
+    override init() {
+        super.init()
+        central = CBCentralManager(delegate: self, queue: .main)
+    }
+    
+    func startScan() {
+        beginScanning()
+    }
+    
+    private func beginScanning() {
+        guard central.state == .poweredOn, !isScanning else { return }
+        isScanning = true
+        central.scanForPeripherals(withServices: nil)
+    }
+}
+
+extension BluetoothManager: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .poweredOn: authorization = .allowed
+        case .unauthorized: authorization = .denied
+        case .poweredOff: authorization = .poweredOff
+        case .unknown, .resetting, .unsupported:
+            authorization = .unknown
+     
+        @unknown default:
+            authorization = .unknown
+        }
+        beginScanning()
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print(peripheral.name)
+    }
 }
