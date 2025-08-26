204d520ec9529f187532f15064abd907b972a66d
Niv BP <nivbp7@gmail.com>
2025-08-26 10:51:10 +0300

    Initial commit

---
+++ PlayerMakerBTScan/PlayerMakerBTScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerMakerBTScanApp.swift
+//  PlayerMakerBTScan
+//
+//  Created by niv ben-porath on 26/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlayerMakerBTScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
