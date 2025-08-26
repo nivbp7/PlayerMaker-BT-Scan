c0b100b47828c1ddd66b5a6df3cbc22bb474c21d
Niv BP <nivbp7@gmail.com>
2025-08-26 13:49:32 +0300

    Add ScanView

---
+++ PlayerMakerBTScan/GUI/Views/ScanScreen/ScanView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanView.swift
+//  PlayerMakerBTScan
+//
+//  Created by niv ben-porath on 26/08/2025.
+//
+
+import SwiftUI
+
+struct ScanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanView()
+}
