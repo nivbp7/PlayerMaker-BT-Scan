6a3b2d36daa595ebbb09ea9c47aad14f791c9284
Niv BP <nivbp7@gmail.com>
2025-08-27 10:23:46 +0300

    Add file

---
+++ PlayerMakerBTScan/GUI/Presentation/ScannerViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ScannerViewModel.swift
+//  PlayerMakerBTScan
+//
+//  Created by niv ben-porath on 27/08/2025.
+//
+
+import Foundation
+
+final class ScannerViewModel: ObservableObject {
+    
+}
